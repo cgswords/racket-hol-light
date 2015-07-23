ebdd2673655e34a9b55662827414923a35cde2e7
Cameron Swords <cswords@mithrandir.local>
2015-07-23 17:39:58 -0400

    Started this stuff

--- fusion.rkt
+++ fusion.rkt
@@ -19,14 +19,14 @@
   (let ([x test])
     (if x x (error msg))))
 
-(define (union l s)
-  (foldr (lambda (x ls) (if (memq x ls) ls (cons x ls))) s l))
+;; (define (union l s)
+;;   (foldr (lambda (x ls) (if (memq x ls) ls (cons x ls))) s l))
 
 ;; Looks up based on the cdr of the pair in the assoc list, where the last
 ;; argument is a default value if it doesn't fine one.
 (define (rev_assocd a l d)
   (cond
-    [(findf (lambda (y) (eq? a (cdr y))) l) => car]
+    [(findf (lambda (y) (equal? a (cdr y))) l) => car]
     [else d]))
 
 ;; Kick off the initial types
@@ -39,7 +39,7 @@
 ;; assoc in ML sort of had this behaviour, but they use 'can', so I sorta need this
 (define (get_type_arity s) 
   (cond
-    [(assq s (types)) => cdr]
+    [(assoc s (types)) => cdr]
     [else #f])) 
 
 ;; Delcare a new type
@@ -78,7 +78,7 @@
 ;; itlist appears to be fold
 (define (tyvars ty)
   (match ty
-    [(tyapp s args) (foldr union '() (map tyvars args))]
+    [(tyapp s args) (foldr set-union '() (map tyvars args))]
     [(tyvar v)      (list v)]))
 
 ;; Substitute types for type variables
@@ -106,7 +106,7 @@
 ;; Gets type of constant
 (define (get_const_type s)
   (cond
-    [(assq s (constants)) => cdr]
+    [(assoc s (constants)) => cdr]
     [else #f]))
 
 ;; Declare a new constant
@@ -171,19 +171,19 @@
   (match term
     [(var      name type) (list term)] 
     [(constant name type) '()]
-    [(comb s t)           (union (frees s) (frees t))]
+    [(comb s t)           (set-union (frees s) (frees t))]
     [(lam_abs  bv bod)    (remove bv (frees bod))]))
 
-(define (freesl termlist) (foldr union '() (map frees termlist)))
+(define (freesl termlist) (foldr set-union '() (map frees termlist)))
 
 ;; Whether all the free variables in a term appear in the list
 
-(define (frees_in acc term)
+(define (freesin acc term)
   (match term
     [(var      name type) (member term acc)] 
     [(constant name type) #t]
-    [(comb s t)           (and (frees acc s) (frees acc t))]
-    [(lam_abs  bv bod)    (frees_in (cons bv acc) bod)]))
+    [(comb s t)           (and (freesin acc s) (freesin acc t))]
+    [(lam_abs  bv bod)    (freesin (cons bv acc) bod)]))
 
 ;; Whether a variable (or constant in fact) is free in a term
 (define (vfree_in v term)
@@ -198,15 +198,251 @@
   (match term
     [(var      name type)     (tyvars type)] 
     [(constant name type)     (tyvars type)]
-    [(comb s t)               (union (type_vars_in_term s) (type_vars_in_term t))]
-    [(lam_abs (var x ty) bod) (union (tyvars ty) (type_vars_in_term bod))]))
+    [(comb s t)               (set-union (type_vars_in_term s) (type_vars_in_term t))]
+    [(lam_abs (var x ty) bod) (set-union (tyvars ty) (type_vars_in_term bod))]))
 
 ;; For name-carrying syntax, we need this early
 
 (define (variant avoid v)
   (cond
-    [(not (ormap (lambda (t) (vfree_in v t)) avoid)) v]
+    [(not (ormap (lambda (t) (vfree_in v t)) avoid)) v] ;; (not (exists (vfree_in v) avoid)
     [else (match v
             [(var s ty) (variant avoid (var (string-append s "'") ty))]
             [else (error "variant: not a variable")])]))
+
+;; Substitution primitive (for variables only)
+(define vsubst
+  (letrec 
+    ([vsubst (lambda (ilist term)
+               (match term
+                 [(var      name type)     (rev_assocd term ilist term)] 
+                 [(constant name type)     term]
+                 [(comb s t)               (comb (vsubst ilist s) (vsubst ilist t))]
+                 [(lam_abs (var v ty) bod) 
+                  (let ([ilist^ (filter (lambda (tx) (not (equal? (cdr tx) v)) ilist))])
+                    (if (null? ilist^)
+                        term
+                        (let ([bod^ (vsubst ilist^ bod)])
+                          (cond
+                            [(equal? bod^ bod) term]
+                            [(ormap (lambda (tx) (and (vfree_in v (car tx)) (vfree_in (cdr tx) bod))) 
+                                    ilist^)
+                             (let ([v^ (variant (list bod^) v)])
+                               (lam_abs v^ (vsubst `((,v^ . ,v) . ,ilist^) bod)))]
+                            [else (abs v bod^)]))))]))])
+    (lambda (theta term)
+      (cond
+        [(null? theta) term]
+        [(andmap (lambda (tx) (equal? (type_of (car tx) ) (var-type (cdr tx)))) theta) 
+         (vsubst theta term)]
+        [else (error "vsubst: Bad substitution list")]))))
+
+
+;; type instantiation primitive
+
+(define (inst tyin term)
+  (letrec
+    ([inst (lambda (env tyin term) 
+             (match term
+               [(var n ty)      (let ([term^ (var n (type_subst tyin ty))])
+                                  (if (equal? (rev_assocd term^ env term) term) 
+                                      term^
+                                      (raise term^)))]
+               [(constant c ty) (constant c (type_subst tyin ty))]
+               [(comb f x)      (comb (inst env tyin f) (inst env tyin x))]
+               [(lam_abs y t)   (let* ([y^ (inst '() tyin y)]
+                                       [env^ `((,y . ,y^) . ,env)]) 
+                                  (with-handlers 
+                                    ([var? (lambda (w^) 
+                                             (if (not (equal? w^ y^))
+                                                 (raise w^)
+                                                 (let* ([ifrees (map (lambda (n) (inst '() tyin n)) (frees t))]
+                                                        [y^^ (variant ifrees y^)]
+                                                        [z (var (car (dest_var y^^)) (cdr (dest_var y)))])
+                                                   (inst env 
+                                                         tyin 
+                                                         (lam_abs z (vsubst `((,z . ,y)) t))))))])
+                                    (lam_abs y^ (inst env^ tyin t))))]))])
+    (cond
+      [(null? tyin) term]
+      [else (inst '() tyin term)])))
+
+;; A few bits of general derived syntax
+
+(define rator comb-term1)
+(define rand  comb-term2)
+
+;; Syntax operators for equations
+
+(define (safe_mk_eq l r)
+  (let ([ty (type_of l)])
+    (comb 
+      (comb 
+        (constant "=" (tyapp "fun" (list ty (tyapp "fun" (list ty bool_ty))))) 
+        l) 
+      r)))
+
+(define (dest_eq term) 
+  (match term
+    [(comb (comb (constant "=" ty) l) r) (cons l r)]
+    [else (error "dest_eq")]))
+
+;; Useful to have term union modulo alpha-conversion for assumption lists (Wat)
+
+(define alphaOrder (lambda (x y) 0))
+(define term_union list)
+(define term_image list)
+
+;; Basic theorem destructors
+
+(define (dest_thm thm)
+  (match thm
+    ([sequent asl c] (cons asl c))))
+
+(define (hyp thm)
+  (match thm
+    ([sequent asl c] asl)))
+
+(define (concl thm)
+  (match thm
+    ([sequent asl c] c)))
+
+;; Basic equality properties. Trans is derivable but included for being fast while I fight
+
+(define (REFL term) (sequent '() (safe_mk_eq term term)))
+
+(define (TRANS s1 s2)
+  (match (list s1 s2)
+    [(list (sequent asl1 c1) (sequent asl2 c2))
+     (let* ([m1  (rand c1)]
+            [eql (rator c1)]
+            [m2r (dest_eq c2)]
+            [m2  (car m2r)]
+            [r   (cdr m2r)])
+      (if (zero? (alphaOrder m1 m2))
+          (sequent (term_union asl1 asl2) (comb eql r))
+          (raise "TRANS")))]
+     [else (raise "TRANS")]))
+
+;; Congruence properties of equality
+
+(define (MK_COMB s1 s2)
+  (match (list s1 s2)
+    [(list (sequent asl1 c1) (sequent asl2 c2))
+     (let* ([l1r1 (dest_eq c1)]
+            [l2r2 (dest_eq c2)]
+            [l1 (car l1r1)]
+            [r1 (cdr l1r1)]
+            [l2 (car l2r2)]
+            [r2 (cdr l2r2)])
+       (match (type_of l1)
+         [(tyapp "fun" `(,ty . ,_)) 
+          (if (equal? (type_of l2) ty) 
+              (sequent (term_union asl1 asl2) (safe_mk_eq (comb l1 l2) (comb r1 r2)))
+              (raise "MK_COMB: types do not agree"))]
+         [else (raise "MK_COMB: not both equations")]))]))
+
+(define (ABS v s)
+  (match s
+    [(sequent asl c)
+     (let* ([lr (dest_eq c)]
+            [l  (car lr)]
+            [r  (cdr lr)])
+       (if (not (ormap (lambda (t) (vfree_in v t)) asl)) ;; not (exists (vfree_in v) asl)
+           (sequent asl (safe_mk_eq (lam_abs v l) (lam_abs v r)))
+           (raise "ABS")))]
+    [else (raise "ABS")]))
+
+;; Trivial case of lambda calculus beta-conversion
+
+(define (BETA term)
+  (match term
+    [(comb (lam_abs v bod) arg)
+     (if (equal? v arg)
+         (sequent '() (safe_mk_eq term bod))
+         (raise "BETA: not a trivial beta-redex"))]
+    [else (raise "BETA: not a trivial beta-redex")]))
+
+;; Rules connected with deduction
+
+(define (ASSUME term)
+  (if (equal? (type_of term) bool_ty)
+      (sequent (list term) term)
+      (raise "ASSUME: not a proposition")))
+
+;; Type and term instantiation
+
+(define (INST_TYPE theta s)
+  (let ([inst_fn (lambda (x) (inst theta x))])
+    (sequent (term_image inst_fn (sequent-terms s)) (inst_fn (sequent-term s)))))
+
+(define (INST theta s)
+  (let ([inst_fn (lambda (x) (vsubst theta x))])
+    (sequent (term_image inst_fn (sequent-terms s)) (inst_fn (sequent-term s)))))
+
+;; Handling of axioms
+
+(define the_axioms (box '())) ;; Theorem list
+
+(define (axioms) (unbox the_axioms))
+
+(define (new_axiom term)
+  (if (equal? (type_of term) bool_ty)
+      (let ([thm (sequent '() term)])
+        (set-box! the_axioms (cons (sequent '() term) (axioms)))
+        thm)
+      (raise "new_axiom: Not a proposition")))
+
+;; Handling of (term) definitions
+
+(define the_definitions (box '())) ;; Theorem list
+
+(define (definitions) (unbox the_definitions))
+
+(define (new_basic_definition term)
+  (match term
+    [(comb (comb (constant "=" _) (var cname ty)) r)
+     (cond
+       [(not (freesin '() r)) (raise "new_definition: term not closed")]
+       [(not (subset? (type_vars_in_term r) (tyvars ty))) ;; should write subset
+        (raise "new_definition: Type variables not reflected in constant")]
+       [else (let* ([c (begin (new_constant cname ty) (constant cname ty))]
+                    [dth (sequent '() (safe_mk_eq c r))])
+               (set-box! the_definitions (cons dth (definitions)))
+               dth)])]
+     [else (raise "new_basic_definition: malformed input")]))
+
+;; Handling of type definitions
+
+(define (new_basic_type_definition tyname absxrep sqt)
+  (let ([absname (car absxrep)]
+        [repname (cdr absxrep)])
+    (match sqt
+      [(sequent asl c)
+       (cond
+        [(ormap get_const_type (list absname repname)) 
+         (raise "new_basic_type_definition: Constant(s) already in use")]
+        [(not (equal? asl '())) 
+         (raise "new_basic_type_definition: Assumptions in theorem")]
+        [else (let* ([Px (dest_comb c)]
+                     [P  (car Px)]
+                     [x  (cdr Px)])
+                (if (not (freesin '() P))
+                    (raise "new_basic_type_definition: Predicate is not closed")
+                    (let* ([tyvars  (sort (type_vars_in_term P) string<=?)] ;; Why do we sort them?
+                           [nothing (new_type tyname (length tyvars))]
+                           [aty (tyapp tyname tyvars)]
+                           [rty (type_of x)]
+                           [absty (tyapp "fun" (list rty aty))]
+                           [repty (tyapp "fun" (list aty rty))]
+                           [abs   (begin (new_constant absname absty) (constant absname absty))]
+                           [rep   (begin (new_constant repname repty) (constant repname repty))]
+                           [a (var "a" aty)]
+                           [r (var "r" rty)])
+                      (cons
+                        (sequent '() (safe_mk_eq (comb abs (mk_comb rep a)) a))
+                        (sequent '() (safe_mk_eq (comb P r) 
+                                                 (safe_mk_eq (mk_comb rep (mk_comb abs r)) r)))))))])])))
+
+;; Stuff that doesn't seem worth putting in (?)
 
